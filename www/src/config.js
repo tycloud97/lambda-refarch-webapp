829c8859f531f40a2735100958de8267fa82ad12
Your Name <you@example.com>
2021-08-14 00:24:19 +0700

    add code

--- www/src/config.js
+++ www/src/config.js
@@ -5,7 +5,7 @@ const config = {
   "aws_user_pools_web_client_id": "5u0q954nemam8dbb2r3rlsi86a",     // CognitoClientID
   "api_base_url": "https://5hhsuyjwch.execute-api.ap-southeast-1.amazonaws.com/prod",                                     // TodoFunctionApi
   "cognito_hosted_domain": "mytodoappdemo-sam.auth.ap-southeast-1.amazoncognito.com",                   // CognitoDomainName
-  "redirect_url": "https://master.d1a8wmm2zcdrwd.amplifyapp.com"                                      // AmplifyURL
+  "redirect_url": "https://master.d384cca881s4dh.amplifyapp.com"                                      // AmplifyURL
 };
 
 export default config;
